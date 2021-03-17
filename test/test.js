ae5c74fbe7488d3d3ba066ab8201d7f7823efab4
qian <zq_sdk@163.com>
2021-03-17 22:50:45 +0800

    add more test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,14 @@
 const fs = require('fs');
-const 原力 = require('../lib');
+const 大能 = require('../lib');
 
-const 函数 = fs.readFileSync(`${__dirname}/operator.yy`, 'utf-8');
+const 操作符 = fs.readFileSync(`${__dirname}/operator.yy`, 'utf-8');
 const 声明 = fs.readFileSync(`${__dirname}/statements.yy`, 'utf-8');
+const 特征 = fs.readFileSync(`${__dirname}/class.yy`, 'utf-8');
+const 流程 = fs.readFileSync(`${__dirname}/flow.yy`, 'utf-8');
+const 函数 = fs.readFileSync(`${__dirname}/function.yy`, 'utf-8');
 
-原力.compile(函数);
-原力.compile(声明);
+大能.compile(操作符);
+大能.compile(声明);
+大能.compile(特征);
+大能.compile(流程);
+// 大能.compile(函数);
